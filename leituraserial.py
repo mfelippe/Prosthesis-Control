42312e184bb11322bdc36ce697d64fbfc695097d
Marcos Felippe <mfelippe.bsb@hotmail.com>
2021-05-06 00:05:29 -0300

    código para ler os dados seriados do mpu6050, utilizando os valores de  Pitch, Roll e YAW

--- leituraserial.py
+++ leituraserial.py
@@ -1,8 +1,9 @@
-#--------------------------------------------------------
+# --------------------------------------------------------
 # Código em Python p/ controle e protese
-#MPU6050
+# MPU6050
 # mfelippe.bsb@hotmail.com
-#--------------------------------------------------------
+# --------------------------------------------------------
+from typing import List
 
 import serial
 import matplotlib.pyplot as plt
@@ -10,40 +11,36 @@ import numpy as np
 
 porta_serial = serial.Serial('COM6', 9600);
 print("iniciando leitura dos dados seriados")
+#base para plotagem
 
-#Array de dados do MPU6050
-ACx = []
-ACy = []
-ACz = []
-AGx = []
-AGy = []
-AGz = []
-i=0;
-while i<=100: # loop para captar os primeiros 100 dados
-    leitura_serial = str(porta_serial.readline())#inicia a leitura serial
-    leitura_serial = leitura_serial[2:-5] #remove os caracteris indesejado
-    dado = leitura_serial.split(',') #separa os dados
-    #print(dado)
-    
-    #separando os dados do array dado e convertendo p /inteiro
 
-    #adcionando dados no array
-    #dados do Aceletrometro  
-    ACx.append(float(dado[0]));
-    ACy.append(float(dado[1]));
-    ACz.append(float(dado[2]));
+# Array de dados do MPU6050
+pitch_x = []
+roll_y = []
+yaw_z = []
 
-    #dados do Giroscopio(x,y,z)
-    AGx.append(float(dado[3]));
-    AGy.append(float(dado[4]));
-    AGz.append(float(dado[5]));
-    i=i+1;
+i = 0;
+while i < 1000:  # loop para captar os primeiros 1000 dados
+    leitura_serial = str(porta_serial.readline())  # inicia a leitura serial
+    leitura_serial = leitura_serial[2:-5]  # remove os caracteris indesejado
+    leitura_serial = leitura_serial.split(',')  # separa os dados
+    print(leitura_serial)
+    i = i + 1
 
-print(ACx);
-print(ACy);
-print(ACz);
-print(AGx);
-print(AGy);
-print(AGz);
+    if i > 60:
+        # separando os dados do array dado e convertendo p /inteiro
 
-  
+        # adcionando dados no array
+        pitch_x.append(float(leitura_serial[0]))
+        roll_y.append(float(leitura_serial[1]))
+        yaw_z.append(float(leitura_serial[2]))
+
+plt.plot(pitch_x)
+plt.plot(roll_y)
+plt.plot(yaw_z)
+plt.grid()
+plt.show()
+
+print(pitch_x)
+print(roll_y)
+print(yaw_z)
